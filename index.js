bc61e32f03bf33bb60d6bdb810791d628882edc1
idoctnef <idoctnef@openmailbox.org>
2016-05-21 13:38:36 +0200

    primer commit

---
+++ index.js
@@ -0,0 +1,14 @@
+//http://placehold.it/1920x1080
+
+function OnLoadIndex(){
+  /*$(document).ready(function(e) {
+      $('#HTMLmenu').load('HTMLmenu.html',function(){
+        //
+      });
+    });*/
+    $(document).ready(function(e) {
+        $('#HTMLfooter').load('HTMLfooter.html',function(){
+          //
+        });
+      });
+}
