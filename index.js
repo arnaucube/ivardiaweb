aea03785b6b0e3f2242c3c546fe447eff824a566
idoctnef <idoctnef@openmailbox.org>
2016-06-07 16:30:40 +0200

    textos afegits

--- index.js
+++ index.js
@@ -21,4 +21,13 @@ function OnLoadNosaltres(){
   setTimeout(function(){
     document.getElementsByClassName("own-menuButtons")[0].childNodes[3].className="own-currentPageMenu";
   }, 500);
+}
+
+function OnClickCampanyesDeSuport(){
+  if(document.getElementById("desplegable").className=="own-desplegable")
+  {
+    document.getElementById("desplegable").className="own-desplegableAmagat";
+  }else{
+    document.getElementById("desplegable").className="own-desplegable";
   }
+}
